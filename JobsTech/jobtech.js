d9f7c7588fc8ba70d1ded046ddca82c9d1d8e816
Nileshdeqode22 <nrajput@deqode.com>
2022-06-09 22:05:01 +0530

    minimize loops

--- JobsTech/jobtech.js
+++ JobsTech/jobtech.js
@@ -1,12 +1,31 @@
 const config = require('./jobs.json');
 const tech=require('./technologies.json')
-const object2 = tech.reduce((acc, nxt) => {
-    let timestamp=Date.now()
 
-	const jobsObjs = config.map(obj => ({...obj, "tags":nxt,"processing_timestamp":timestamp}));
-	acc.push(...jobsObjs);
-	return acc;
-}, []);
+const fs = require('fs');
+
+	console.log("conifg ",config.length)
+	const object2 = tech.reduce((acc, nxt) => {
+		// for (let i=0;i<config[i].description.length;i++){
+				let arrayDesc=config[2].description.split(" ")
+
+				
+				let timestamp=Date.now()
+				const jobsObjs = config.map(obj => ({...obj, "tags":nxt,"processing_timestamp":timestamp}));
+				acc.push(...jobsObjs);
+			
+			fs.writeFileSync("./sample_expected_output.json", JSON.stringify(acc,null,30), 'utf8', function (err) {
+				if (err) {
+					return console.log(err);
+				}
+			
+				
+			}); 
+			console.log(acc)
+			return acc;
+		//}
+	}, []);
+
+
 
 
 
