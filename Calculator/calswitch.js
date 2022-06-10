929b2fa6419f295c394c5ff6052670273e1e29aa
Nileshdeqode22 <nrajput@deqode.com>
2022-06-10 17:45:35 +0530

    added modified code for jobtech,calculator

--- Calculator/calswitch.js
+++ Calculator/calswitch.js
@@ -1,50 +1,64 @@
 
-
-
-let readline = require('readline-sync');
-let result
-let decision =false
-
-
-do{
-
-
-  let op = readline.question('Enter operator:');
-
-  let number1 = parseInt(readline.question('Please enter a number: '));
-  let number2 = parseInt(readline.question('Please enter another number: '));
+            let readline = require('readline-sync');
+            let result;
+            let op;
 
 
 
-  switch(op) {
-      case '+':
-          result = number1 + number2;
-          console.log(`${number1} + ${number2} = ${result}`);
-          break;
-
-      case '-':
-          result = number1 - number2;
-          console.log(`${number1} - ${number2} = ${result}`);
-          break;
-
-      case '*':
-          result = number1 * number2;
-          console.log(`${number1} * ${number2} = ${result}`);
-          break;
-
-      case '/':
-          result = number1 / number2;
-          console.log(`${number1} / ${number2} = ${result}`);
-          break;
-
-      default:
-          console.log('Invalid operator');
-          break;
+        readline.question(" \n\n ********sWelcome to the world of Calculator********")
         
-  }
 
- 
-}while(decision!=true)
+
+    for(;;){
+        console.log("\n\nPlease Enter your choice what Operation do you want to perform ?:\n");
+        op = parseInt(readline.question("1-Addition.\n2-Substraction.\n3-Multiplication.\n4-Division.\n5-Exit.\n\n"));
+
+        if (op==5){
+            readline.question("Thank You for using Calculator....")
+            process.exit(1);
+
+        }
+        let number1 = parseInt(readline.question("\n\n Please enter a number:\n "));
+        let number2 = parseInt(readline.question("\n\n Please enter another number:\n "));
+            switch(op) {
+                    case 1:
+                        result = number1 + number2;
+                        console.log(`\n Addition of ${number1} + ${number2} = ${result}`);
+                        break;
+
+                    case 2:
+                        result = number1 - number2;
+                        console.log(`\n Substraction of ${number1} - ${number2} = ${result}`);
+                        break;
+
+                    case 3:
+                        result = number1 * number2;
+                        console.log(`\n Multiplication of ${number1} * ${number2} = ${result}`);
+                        break;
+
+
+                    case 4:
+                        if(number2===0)
+                        {
+                            console.log("OOps Divide by zero\n");
+
+                        }else{
+                            result = number1 / number2;
+                            console.log(`\n Division of ${number1} / ${number2} = ${result}`);
+                        }
+                        break;
+                    default:
+                        console.log("\n Enter correct option \n");
+                        break;
+                        
+                              
+            }
+            
+           
+
+    }
+    
+
 
 
 
--- JobsTech/jobtech.js
+++ JobsTech/jobtech.js
@@ -1,29 +1,24 @@
-const config = require('./jobs.json');
-const tech=require('./technologies.json')
+const jobs = require('./jobs.json');
+const technologies=require('./technologies.json')
 
 const fs = require('fs');
 
-	console.log("conifg ",config.length)
-	const object2 = tech.reduce((acc, nxt) => {
-		// for (let i=0;i<config[i].description.length;i++){
-				let arrayDesc=config[2].description.split(" ")
+for(let info of jobs){
+	//splitt string into array
+	const desc=info.description.split(" ");
+	const matchData=technologies.filter((tech) => desc.includes(tech))
+	info.tags=matchData
+	info.processing_timestamp=Date.now()	
+	
+}
+fs.writeFileSync("./sample_expected_output.json", JSON.stringify(jobs,null,4), 'utf8', function (err) {
+	if (err) {
+			return console.log(err);
+	} 
+			
+				
+}); 
 
-				
-				let timestamp=Date.now()
-				const jobsObjs = config.map(obj => ({...obj, "tags":nxt,"processing_timestamp":timestamp}));
-				acc.push(...jobsObjs);
-			
-			fs.writeFileSync("./sample_expected_output.json", JSON.stringify(acc,null,30), 'utf8', function (err) {
-				if (err) {
-					return console.log(err);
-				}
-			
-				
-			}); 
-			console.log(acc)
-			return acc;
-		//}
-	}, []);
 
 
 
