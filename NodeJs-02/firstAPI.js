5d54010644ed144ee0a757c2a04841a0a363d2b7
Nileshdeqode22 <nrajput@deqode.com>
2022-06-23 01:08:43 +0530

    added get apis using reques and request-promises

--- NodeJs-02/firstAPI.js
+++ NodeJs-02/firstAPI.js
@@ -1,44 +1,62 @@
 //Create a program to fetch content from the API's provided on the following link and store its response in different files in the filesystem.
 //http://dummy.restapiexample.com/
 
-const request = require('request');
+const request=require('request');
+
 const fs = require('fs');
 
-//Create a program to fetch content from the API's provided on the following link and store its response in different files in the filesystem.
 
 //Get request http://dummy.restapiexample.com/api/v1/employees and store in text file name timestamp employee.txt
 
-request.get('https://dummy.restapiexample.com/api/v1/employees', function (error, response, body) {
+const options={
+    url:'https://dummy.restapiexample.com/api/v1/employees',//url
+    method:'GET',//GET is used to fetch data from the server
+    //headers are used to send data to the server
+    headers:{  
+        'User-Agent':'request',
+        'Content-Type':'application/json'
+
+    }
+}
+
+request(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+        fs.writeFileSync(`./output/${Date.now()}_employeealldata.txt`, body, 'utf8');
+    } else {
+        console.log('error');
+    }
+}
+);
+
+
+//get request http://dummy.restapiexample.com/api/v1/employee/1 and store in text file name timestamp employee1.txt
+
+const option = {
+    url: 'https://dummy.restapiexample.com/api/v1/employee/1',
+    method: 'GET',
+    headers: {
+        'User-Agent':'request',
+        'Content-Type': 'application/json'
+    }
+}
+
+
+request(option, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log(body);
         //write the response to a file named with the current timestamp
-        fs.writeFileSync(`${Date.now()}_employee.txt`, body, function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        }
-        );  
-    }else{
-        console.error("API Error: " + error);
+        fs.writeFileSync(`./output/${Date.now()}_employee1.txt`, body, 'utf8');
+    } else {
+        console.error( error);
     }
 }
 );
 
 
-//Get request http://dummy.restapiexample.com/api/v1/employees/1 and store in text file name timestamp employee1.txt
 
-request.get('https://dummy.restapiexample.com/api/v1/employee/1', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body);
-        fs.writeFileSync(`${Date.now()}_employee1.txt`, body, function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        }
-        );
-    }else{
-        console.error("API Error: " + error);
-    }
-}
-);
+
+
+
 
 
 
